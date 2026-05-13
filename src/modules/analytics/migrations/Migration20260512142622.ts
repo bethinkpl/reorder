a3fb9b1bdd9c924fe59d7770c55e617cf809f04e
Michał Czech <michal.czech@tsh.io>
2026-05-14 01:05:43 +0200

    fix: better handling of down migration

--- src/modules/analytics/migrations/Migration20260512142622.ts
+++ src/modules/analytics/migrations/Migration20260512142622.ts
@@ -9,6 +9,8 @@ export class Migration20260512142622 extends Migration {
   }
 
   override async down(): Promise<void> {
+    this.addSql(`delete from "subscription_metrics_daily" where "frequency_interval" = 'day';`);
+
     this.addSql(`alter table if exists "subscription_metrics_daily" drop constraint if exists "subscription_metrics_daily_frequency_interval_check";`);
 
     this.addSql(`alter table if exists "subscription_metrics_daily" add constraint "subscription_metrics_daily_frequency_interval_check" check("frequency_interval" in ('week', 'month', 'year'));`);
--- src/modules/subscription/migrations/Migration20260512142623.ts
+++ src/modules/subscription/migrations/Migration20260512142623.ts
@@ -9,6 +9,8 @@ export class Migration20260512142623 extends Migration {
   }
 
   override async down(): Promise<void> {
+    this.addSql(`update "subscription" set "frequency_interval" = 'week', "frequency_value" = ceil("frequency_value" / 7.0) where "frequency_interval" = 'day';`);
+
     this.addSql(`alter table if exists "subscription" drop constraint if exists "subscription_frequency_interval_check";`);
 
     this.addSql(`alter table if exists "subscription" add constraint "subscription_frequency_interval_check" check("frequency_interval" in ('week', 'month', 'year'));`);
